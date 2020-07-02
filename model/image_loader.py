fix normalization

model/image_loader.py
@@ -2,18 +2,23 @@ from PIL import Image
 import torch
 import torchvision.transforms as transforms
 
-def tensor2PIL(tensor):
+
+def tensor2PIL(tensor, mean, std):
     image = tensor.cpu().clone()
     image = image.squeeze(0)
+    image = image * std.view(-1, 1, 1) + mean.view(-1, 1, 1)
+    image = torch.clamp(image, 0, 1)  
     image = transforms.ToPILImage()(image)
     return image
 
 
 class Image_loader():
     def __init__(self, content_path_list, style_path_list,
-                 img_size, device, print_flg):
+                 img_size, device, mean, std, print_flg):
                
         self.device = device
+        self.mean = mean
+        self.std = std
         self.content_path_list = content_path_list
         self.style_path_list = style_path_list
         self.content_sizes = []
@@ -47,6 +52,7 @@ class Image_loader():
             self.content_sizes.append(image.numpy().shape[-2:])
             
             image = torch.FloatTensor(image)
+            image = self.normalize(image)
             self.content_imgs.append(image)
                     
         # default
@@ -56,7 +62,9 @@ class Image_loader():
         # load style images
         self.load_style_images(self.style_path_list)
         
-    def normalize(self, img, mean, std):
+    def normalize(self, img):
+        mean = self.mean.view(-1, 1, 1)
+        std = self.std.view(-1, 1, 1)
         return (img - mean) / std
         
     def get_content(self):
@@ -73,7 +81,7 @@ class Image_loader():
     def load_style_images(self, path_list):
         
         style_load = transforms.Compose([
-            transforms.Resize(self.curr_content_size),
+            transforms.Resize(self.curr_content_size, Image.LANCZOS),
             transforms.ToTensor()])
         
         self.style_imgs = []
@@ -81,4 +89,5 @@ class Image_loader():
             
             image = Image.open(p)
             image = style_load(image).unsqueeze(0)
+            image = self.normalize(image)
             self.style_imgs.append(image)

model/model.py
@@ -5,6 +5,7 @@ from PIL import Image
 
 import torch
 import torch.nn as nn
+from torch.nn import Conv2d, ReLU, AvgPool2d, Sequential
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.transforms as transforms
@@ -18,20 +19,19 @@ from model.losses import *
 IMG_SIZE = 300
 ITER_NUM = 500
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-STYLE_WEIGHT = 1e7
+STYLE_WEIGHT = 1e5
 TV_STYLE_COEFF = 5e-12
 VGG19_W_PATH = 'vgg19_conv1_1-relu4_2.pt'
 LAYER_NAMES = ['conv1_1', 'relu1_1', 'conv1_2', 'relu1_2', 'pool1',
                'conv2_1', 'relu2_1', 'conv2_2', 'relu2_2', 'pool2',
-               'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2', 'conv3_3',
-                'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
-               'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2', 'conv4_3',
-                'relu4_3', 'conv4_4', 'relu4_4', 'pool4',
-               'conv5_1', 'relu5_1', 'conv5_2', 'relu5_2', 'conv5_3',
-                'relu5_3', 'conv5_4', 'relu5_4', 'pool5']
+               'conv3_1', 'relu3_1', 'conv3_2', 'relu3_2',
+               'conv3_3', 'relu3_3', 'conv3_4', 'relu3_4', 'pool3',
+               'conv4_1', 'relu4_1', 'conv4_2', 'relu4_2']
 CONTENT_LAYERS = ['relu4_2']
 STYLE_LAYERS = ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_2']
 STYLE_LOSS_WEIGHTS = np.array([0.5,.4,.3,.2]) / 1.4
+VGG_MEAN = torch.Tensor([0.485, 0.456, 0.406])
+VGG_STD = torch.Tensor([0.229, 0.224, 0.225])
 
 class Model_vgg19_cut(nn.Module):
 
@@ -172,9 +172,9 @@ def run_style_transfer(image_loader,
             run[0] += 1
             if (run[0] % 100 == 0) and (print_flg):
                 
-                print("Iter {}:".format(run[0]))
-                print('Style Loss : {:4f} Content Loss : {:4f}'.format(
-                    style_score.item(), content_score.item()))
+                print('Iter {}:'.format(run[0]))
+                print('Style Loss : {:.4f} Content Loss: {:.4f} TV Loss: {:.4f}'\
+                      .format(style_score.item(), content_score.item(), tv_score.item()))
                 print('{} sec'.format(int(time.time()-start_time)))
                 print()
 
@@ -190,7 +190,8 @@ def run_style_transfer(image_loader,
 
 async def apply_NST(content_path, style_path, save_path, style_weight):
 
-	image_loader = Image_loader([content_path], [style_path], IMG_SIZE, DEVICE, True)
+	image_loader = Image_loader([content_path], [style_path],
+								IMG_SIZE, DEVICE, VGG_MEAN, VGG_STD, True)
 	input_img = image_loader.get_content()
 
 	output_img = run_style_transfer(image_loader=image_loader,
@@ -217,7 +218,7 @@ if __name__ == '__main__':
     test_style_path = '../test_style.jpg'
     test_output_path = '../test_result.jpg'
     image_loader = Image_loader([test_content_path], [test_style_path],
-    							IMG_SIZE, DEVICE, True)
+    							IMG_SIZE, DEVICE, VGG_MEAN, VGG_STD, True)
     input_img = image_loader.get_content().clone()
     
     output_img = run_style_transfer(image_loader=image_loader,
@@ -232,5 +233,5 @@ if __name__ == '__main__':
                                     tv_weight=STYLE_WEIGHT*TV_STYLE_COEFF,
                                     print_flg=True)
 
-    tensor2PIL(output_img).save(test_output_path)
+    tensor2PIL(output_img, VGG_MEAN, VGG_STD).save(test_output_path)
 